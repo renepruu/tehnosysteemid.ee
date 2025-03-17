92b9ff690007efdfc7f3e9e24d3a78dac85b1157
rene <prene@tlu.ee>
2025-03-17 19:22:12 +0200

    changed base

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tehnosysteemid.ee/', // Make sure this matches your repo name
+  base: process.env.DEPLOY_ENV === 'github' ? '/tehnosysteemid.ee/' : '/', // Use '/tehnosysteemid.ee/' for GitHub Pages, '/' for Netlify
   plugins: [react()],
   build: {
     minify: true,
